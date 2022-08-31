e5ab573baca5626f2913b2118087a08d53e571ce
Camille Rosewright <camille.rosewright@gmail.com>
2022-09-01 00:41:58 +0900

    :bug: Fix inability to exit program with ctrl+c

--- commit.sh
+++ commit.sh
@@ -17,22 +17,22 @@ fi
 
 
 # Ask for the commit type
-COMMIT_TYPE_KEY=$(gum choose "${!commit_types[@]}")
+COMMIT_TYPE_KEY=$(gum choose "${!commit_types[@]}") || exit 1
 COMMIT_TYPE=${commit_types[$COMMIT_TYPE_KEY]}
 
 # Example scope: feat(lang): add Japanese language
 # See https://www.conventionalcommits.org/en/v1.0.0/ for more examples / references
-SCOPE=$(gum input --placeholder "$COMMIT_TYPE(scope)")
+SCOPE=$(gum input --placeholder "$COMMIT_TYPE(scope)") || exit 1
 
 # Scope is optional - if it exists, wrap it in parentheses
 test -n "$SCOPE" && SCOPE="($SCOPE)"
 
 # Pre-populate the input with the commit type and scope
-SUMMARY=$(gum input --prompt "> $COMMIT_TYPE$SCOPE: " --placeholder "Summary of this change")
+SUMMARY=$(gum input --prompt "> $COMMIT_TYPE$SCOPE: " --placeholder "Summary of this change") || exit 1
 while [ -z $SUMMARY ]; do
-  SUMMARY=$(gum input --prompt "> $COMMIT_TYPE$SCOPE: " --placeholder "Seriously, what is the summary of this change?")
+  SUMMARY=$(gum input --prompt "> $COMMIT_TYPE$SCOPE: " --placeholder "Seriously, what is the summary of this change?") || exit 1
 done
-DESCRIPTION=$(gum write --placeholder "Details of this change (CTRL+D to finish)")
+DESCRIPTION=$(gum write --placeholder "Details of this change (CTRL+D to finish)") || exit 1
 
 # Commit the changes
 gum confirm "Commit changes?" && git commit -m "$COMMIT_TYPE$SCOPE: $SUMMARY" -m "$DESCRIPTION"
