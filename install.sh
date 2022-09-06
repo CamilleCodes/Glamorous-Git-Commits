38fe7651ee5d096103d203240c4d03049dd5785b
Camille Rosewright <camille.rosewright@gmail.com>
2022-09-06 18:49:44 +0900

    :wrench:  (installer): Add function to clone GGC repository

--- install.sh
+++ install.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 { # This ensures the entire script is downloaded #
 
-  echo "Hello, world"
+
+  declare -r GGC_REPO="https://github.com/envpcamille/Glamorous-Git-Commits.git"
+
+
+  declare -r XDG_DATA_HOME="${XDG_DATA_HOME:-"$HOME/.local/share"}"
+  declare -r XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-"$HOME/.config"}"
+
+  declare -r GGC_BASE_DIR="${GGC_BASE_DIR:-"$XDG_DATA_HOME/ggc"}"
+  declare -r GGC_CONFIG_DIR="${GGC_CONFIG_DIR:-"$XDG_CONFIG_HOME/ggc"}"
 
   # Check for required dependencies
   function check_system_dependencies() {
@@ -15,4 +23,22 @@
       exit 1
     fi
   }
+
+  # Clone repository
+  function clone_ggc() {
+    echo "Cloning repository: https://github.com/envpcamille/Glamorous-Git-Commits"
+
+    if ! git clone "$GGC_REPO" "$GGC_BASE_DIR"; then
+      echo "Failed to clone repository. Installation failed."
+      exit 1
+    fi
+   }
+
+   function main() {
+     echo "Hello, world"
+     check_system_dependencies
+     clone_ggc
+   }
+
+   main
 } # This ensures the entire script is downloaded # 
