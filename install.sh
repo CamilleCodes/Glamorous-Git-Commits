ad05fa14fa44f5c0a989e78d7a0b15751e862188
Camille Rosewright <camille.rosewright@gmail.com>
2022-09-06 18:28:21 +0900

    :wrench:  (installer): Add function to check dependencies

--- install.sh
+++ install.sh
@@ -2,4 +2,17 @@
 { # This ensures the entire script is downloaded #
 
   echo "Hello, world"
+
+  # Check for required dependencies
+  function check_system_dependencies() {
+    # Git & Gum
+    if ! command -v git &>/dev/null; then
+      echo "Missing required dependency: Git"
+      exit 1
+    fi
+    if ! command -v gum &>/dev/null; then
+      echo "Missing required dependency: Gum"
+      exit 1
+    fi
+  }
 } # This ensures the entire script is downloaded # 
